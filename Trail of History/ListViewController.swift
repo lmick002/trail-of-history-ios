ee694dc787f3ab93ce9721153b2611261c47fd48
Larry Mickie <lmick002@gmail.com>
2016-09-04 01:30:23 -0400

    Model Class

--- Trail of History/ListViewController.swift
+++ Trail of History/ListViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ListViewController: UICollectionViewController {
 
+    var pointOfInterests: [PointOfInterest]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
---
+++ Trail of History/PointOfInterest.swift
@@ -0,0 +1,19 @@
+//
+//  PointOfInterest.swift
+//  Trail of History
+//
+//  Created by Larry Mickie on 9/4/16.
+//  Copyright © 2016 CLT Mobile. All rights reserved.
+//
+
+import UIKit
+
+class PointOfInterest: NSObject {
+    var details: String?
+    var imageUrl: NSURL?
+    var latitude: Float?
+    var longititude: Float?
+    var name: String?
+    var uid: String?
+
+}
